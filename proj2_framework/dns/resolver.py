4696329fc51f482a1c791b04fb94beb3925c9f54
OsthatoAlfakyn <jan.potma@gmail.com>
2016-05-19 16:26:36 +0200

    resolver rootserver loop

--- proj2_framework/dns/resolver.py
+++ proj2_framework/dns/resolver.py
@@ -42,10 +42,57 @@ class Resolver(object):
         Returns:
             (str, [str], [str]): (hostname, aliaslist, ipaddrlist)
         """
-        timeout = 2 # the time waited for a response
+        
+        ROOT_SERVERS = [
+        '198.41.0.4',
+        '192.228.79.201',
+        '192.33.4.12',
+        '199.7.91.13',
+        '192.203.230.10',
+        '192.5.5.241',
+        '192.112.36.4',
+        '198.97.190.53',
+        '192.36.148.17',
+        '192.58.128.30',
+        '193.0.14.129',
+        '199.7.83.42',
+        '202.12.27.33'
+        ]
+
+        timeout = 2 # Time waited for a response
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         sock.settimeout(timeout)
 
+        # Create query
+        question = dns.message.Question(hostname, Type.A, Class.IN)
+        header = dns.message.Header(4242, 0, 1, 0, 0, 0)
+        header.qr = 0
+        header.opcode = 0
+        header.rd = 0 # Iterative (1 is recursive)
+        query = dns.message.Message(header, [question])
+        
+        responseget = False
+        
+        serverindex = 0
+        while(!responseget && serverindex <= len(ROOT_SERVERS)){
+            serverinquestion = ROOT_SERVERS[serverindex]
+            
+            # Send query
+            sock.sendto(query.to_bytes(), (serverinquestion, 53))
+            
+            # Receive response
+            data = sock.recv(512)
+            response = dns.message.Message.from_bytes(data)
+            if(response is not None)
+                responseget = True
+            else
+                serverindex++
+        }
+        
+        
+        
+        """ \/ EXAMPLE \/
+        
         # Create and send query
         question = dns.message.Question(hostname, Type.A, Class.IN)
         header = dns.message.Header(9001, 0, 1, 0, 0, 0)
@@ -70,3 +117,4 @@ class Resolver(object):
                 addresses.append(answer.rdata.data)
 
         return hostname, aliases, addresses
+        """
